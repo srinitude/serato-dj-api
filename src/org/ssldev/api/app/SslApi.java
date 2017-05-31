a5687ef0cdde316a6abae3b1655ef5c66e8076a0
Elad <djelad00@gmail.com>
2017-05-30 23:50:46 -0700

    load app configuration via properties file

---
+++ src/org/ssldev/api/app/SslApi.java
@@ -0,0 +1,111 @@
+package org.ssldev.api.app;
+
+import org.ssldev.api.services.DataConsumedPublisherService;
+import org.ssldev.api.services.DomService;
+import org.ssldev.api.services.NowPlayingGuiService;
+import org.ssldev.api.services.SslByteConsumerService;
+import org.ssldev.api.services.SslCurrentSessionFinderService;
+import org.ssldev.api.services.SslFileReaderService;
+import org.ssldev.api.services.SslInProgressStartupService;
+import org.ssldev.api.services.SslSessionFileUnmarshalService;
+import org.ssldev.api.services.TrackPublisherService;
+import org.ssldev.core.messages.MessageIF;
+import org.ssldev.core.mgmt.AsyncEventHub;
+import org.ssldev.core.mgmt.EventHub;
+import org.ssldev.core.services.FileWriterService;
+import org.ssldev.core.services.ServiceIF;
+import org.ssldev.core.utils.Logger;
+
+public class SslApi {
+
+	private String domFilePath = null;
+	private String pathToSeratoFolder = null;
+	private static String configFileName = "sslApiConfiguration.properties";
+	private static boolean showGui = false;
+	
+	private static SslApi instance = null;
+	private EventHub hub; 
+
+	public static void main(String[] args) {
+		SslApi m = new SslApi();
+
+		m.init(new AsyncEventHub(3));
+	}
+	
+	public void init(EventHub hub) {
+		// laod the configuration
+		SslApiConfig.load(configFileName);
+		System.out.println(SslApiConfig.instance.toString());
+
+		showGui = SslApiConfig.getBooleanProperty(SslApiConfig.Property.START_GUI);
+		
+		// current threading model requires 3 threads (more threads may cause out-of-order notifications; current implementation does not guarantee order)
+		// 1 event thread for GUI
+		// 1 SSL directory/file change listener
+		// 1 general worker thread to service the blocking event queue
+		int numThreads = showGui? 3 : 2;
+		
+		// defaultify the event hub if none is provided
+		this.hub = hub == null? new AsyncEventHub(numThreads) : hub;
+		
+		// setup the logger
+		Logger.enableDebug(SslApiConfig.getBooleanProperty(SslApiConfig.Property.ENABLE_DEBUG)); 
+		Logger.enableFinest(SslApiConfig.getBooleanProperty(SslApiConfig.Property.ENABLE_FINEST)); 
+		Logger.enableTrace(SslApiConfig.getBooleanProperty(SslApiConfig.Property.ENABLE_TRACE));
+		Logger.setShowTime(SslApiConfig.getBooleanProperty(SslApiConfig.Property.SHOW_TIME));
+		
+		if(SslApiConfig.getBooleanProperty(SslApiConfig.Property.ENABLE_LOG_TO_FILE)) {
+			String logFilePath = SslApiConfig.getStringProperty(SslApiConfig.Property.LOG_FILE_PATH);
+			Logger.init(logFilePath);
+		}
+		
+		domFilePath = SslApiConfig.getStringProperty(SslApiConfig.Property.DOM_FILE_PATH);
+		pathToSeratoFolder = SslApiConfig.getStringProperty(SslApiConfig.Property.SSL_DIR_PATH);
+		
+		SslApi.instance = this;
+		
+		// register all app services
+		registerAllServices(this.hub);
+	}
+	
+	/**
+	 * allow shutdown of api
+	 */
+	public static void shutdown() {
+		if(null != SslApi.instance) {
+			SslApi.instance.hub.shutdown();
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	private void registerAllServices(EventHub hub) {
+		// create all services 
+		SslFileReaderService readerService = new SslFileReaderService(hub);
+		SslSessionFileUnmarshalService sslUnmarshalService = new SslSessionFileUnmarshalService(hub); 
+		SslByteConsumerService consumerService = new SslByteConsumerService(hub);
+		FileWriterService writerService = new FileWriterService(hub);
+		DataConsumedPublisherService dataPubService = new DataConsumedPublisherService(hub);
+		DomService dom = new DomService(hub, domFilePath);
+		SslCurrentSessionFinderService sslFileService = new SslCurrentSessionFinderService(hub, pathToSeratoFolder );
+		TrackPublisherService decks = new TrackPublisherService(hub);
+		SslInProgressStartupService lateStartService = new SslInProgressStartupService(hub);
+		
+		// optional service to provide a GUI display for what songs
+		// currently loaded/playing
+		if(showGui) {
+			NowPlayingGuiService gui = new NowPlayingGuiService(hub);
+		}
+	}
+	
+	/**
+	 * register this service to be notified of given message/event set.  If no messages are given, the service
+	 * will be notified of all messages/events.
+	 * 
+	 * @param externalService to notify
+	 * @param msgs optional set of messags to be notified of
+	 */
+	@SafeVarargs
+	public final void register(ServiceIF externalService, Class<? extends MessageIF>... msgs){
+		hub.register(externalService, msgs);
+	}
+}
---
+++ src/org/ssldev/api/app/SslApiConfig.java
@@ -0,0 +1,134 @@
+package org.ssldev.api.app;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.ssldev.core.utils.Logger;
+import org.ssldev.core.utils.SysInfo;
+
+/**
+ * manages app configuration.  App should not run if configuration cannot be loaded
+ */
+public class SslApiConfig {
+	private final static Properties config = new Properties();
+	private final static String sslApiDir = ".sslapi";
+	private final static String EMPTY = "";
+	private final String defaultConfig = "sslApiDefaultConfiguration.properties";
+	private String configAsString;
+	public static SslApiConfig instance;
+	
+	protected static enum Property {
+		ENABLE_DEBUG("enableLoggerDebug"),
+		ENABLE_FINEST("enableLoggerFinest"),
+		ENABLE_TRACE("enableLoggerTrace"),
+		ENABLE_LOG_TO_FILE("enableLogToFile"),
+		SHOW_TIME("enableLoggerTimeStamp"),
+		START_GUI("startGui"),
+		LOG_FILE_PATH("logFilePath"),
+		DOM_FILE_PATH("domFilePath"),
+		SSL_DIR_PATH("sslFolderPath");
+		
+		String key;
+		private Property(String val) {
+			key = val;
+		}
+	}
+	
+	/**
+	 * load the app configuration file
+	 * 
+	 * @param configFileName name of the configuration file to be loaded by the classloader
+	 * @throws IllegalStateException if the configuration file cannot be found or loaded
+	 */
+	private SslApiConfig(String configFileName) {
+		// load default configuration
+		InputStream defaultStream = SslApiConfig.class.getClassLoader().getResourceAsStream(defaultConfig);
+		if (null == defaultStream) {
+			throw new IllegalStateException("cannot find default configuration file " + defaultConfig);
+		}
+		try {
+			config.load(defaultStream);
+		} catch (IOException e) {
+			Logger.warn(this, "could not load the app configuration file: "+configFileName+".  Will attempt to use default values instead");
+		}
+		
+		// load app configuration - mappings override default values
+		InputStream appConfigStream = SslApiConfig.class.getClassLoader().getResourceAsStream(configFileName);
+		if (null == appConfigStream) {
+			throw new IllegalStateException("cannot find app configuration file " + configFileName);
+		}
+		try {
+			config.load(appConfigStream);
+		} catch (IOException e) {
+			Logger.warn(this, "could not load the app configuration file: "+configFileName+".  Will attempt to use default values instead");
+		}
+		
+		// cleanup IO
+		try {
+			appConfigStream.close();
+			defaultStream.close();
+		} catch (IOException e) {
+			Logger.error(this, "could not close the configuration IO stream due to " + e);
+			e.printStackTrace();
+		}
+		
+		// defaultify properties not set in default config
+		defaultifyIfAbsent();
+	}
+	
+	private void defaultifyIfAbsent() {
+		// default paths - could not be set via default config prop file
+		if(config.getProperty(Property.LOG_FILE_PATH.key) == null) {
+			config.setProperty(Property.LOG_FILE_PATH.key, SysInfo.getUserHomeLoc()+sslApiDir+File.separator+"log.txt");
+		}
+		if(config.getProperty(Property.DOM_FILE_PATH.key) == null) {
+			config.setProperty(Property.DOM_FILE_PATH.key, SysInfo.getUserHomeLoc()+sslApiDir+File.separator+"ssldom.txt");
+		}
+		if(config.getProperty(Property.SSL_DIR_PATH.key) == null) {
+			config.setProperty(Property.SSL_DIR_PATH.key, SysInfo.getUserHomeLoc() + "Music" + File.separator + "_Serato_" + File.separator);
+		}
+	}
+
+	/**
+	 * load all configuration properties found in given file
+	 * @param configFileName
+	 * @throws IllegalStateException if file cannot be found or loaded
+	 */
+	public static void load(String configFileName) {
+		if(null == instance) {
+			instance = new SslApiConfig(configFileName);
+		}
+	}
+
+	public static boolean getBooleanProperty(Property prop) {
+		String val = config.getProperty(prop.key); 
+		if(null == val) {
+			Logger.warn(SslApiConfig.class, "could not find property "+ prop.key) ;
+			return false;
+		}
+		
+		return Boolean.parseBoolean(val);
+	}
+	
+	public static String getStringProperty(Property prop) {
+		String val = config.getProperty(prop.key);
+		if(null == val) {
+			Logger.warn(SslApiConfig.class, "could not find property "+ prop.key) ;
+			return EMPTY;
+		}
+		
+		return val;
+	}
+
+	@Override
+	public String toString() {
+		if(null == configAsString) {
+			StringBuilder sb = new StringBuilder(getClass().getSimpleName());
+			sb.append("\n").append(config);
+			configAsString = sb.toString();
+		}
+		return configAsString;
+	}
+}
